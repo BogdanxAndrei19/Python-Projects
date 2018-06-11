47a0b4fe91f16d04b8aa9ad46db0f7509cfa3a85
Bogdan Andrei <bogdanalexandruandrei19@gmail.com>
2018-06-11 08:25:06 +0300

    Update and rename Profanity Checker checker.py to Profanity Checker/profanityChecker.py

--- Profanity Checker/profanityChecker.py
+++ Profanity Checker/profanityChecker.py
@@ -1,7 +1,7 @@
 import urllib
 
 def readText():
-    quotes=open("C:\Bogdan's Folder\#INFO\UDACITY\Introduction to functions in Python\ProfanityChecker\movie_quotes.txt")
+    quotes=open("C:\path\movie_quotes.txt")
     contentFile=quotes.read()
     print(contentFile)
     quotes.close()
